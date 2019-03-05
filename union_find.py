bdfc66ef3715049490ea145d4a0cc942e501f828
Daiki Tanaka <daiki.yosky@gmail.com>
2019-03-05 13:40:15 +0900

    update

--- union_find.py
+++ union_find.py
@@ -1,3 +1,5 @@
+# Union-Find structure
+
 class UnionFind():
     def __init__(self, size):
         # self.parent holds a list of idx for parant node of i-th node
@@ -25,30 +27,30 @@ class UnionFind():
             self.parent[i] = self.find(self.parent[i])
             return self.parent[i]     
         
-    def unite(self, i, j):
+    def unite(self, i_, j_):
         """
         Unite two parents
         Args : 
             i, j : indices of target nodes.
         """ 
         # search roots
-        i = self.find(i)
-        j = self.find(j)
+        i = self.find(i_)
+        j = self.find(j_)
         
         if self.height[i] < self.height[j]:
             self.parent[j] += self.parent[i]
-            self.parent[i] = j
+            self.parent[i] = j_
             
         else:
             self.parent[i] += self.parent[j]
-            self.parent[j] = i
+            self.parent[j] = i_
             if self.height[i] == self.height[j]:
                 self.height[i] += 1
        
     def size(self, i):
         """
-        Get num of nodes in same parent
+        Get num of nodes in the same group
         Args : 
             i : index of target node.
         """
-        return - self.find(i)
+        return - self.parent[self.find(i)]
